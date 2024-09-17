f4387ffb28b464090b0b6352b7c61cefa7c52364
Arpit-Mathur09 <786mathurarpit@gmail.com>
2024-09-17 17:42:36 +0530

    desciption

--- app/layout.tsx
+++ app/layout.tsx
@@ -7,7 +7,7 @@ const inter = Inter({ subsets: ["latin"] });
 
 export const metadata: Metadata = {
   title: "HerbVed",
-  description: "HerbVed by AYUSH ",
+  description: "HerbVed For AYUSH ",
   icons: {
     icon: '/favicon.ico',
   }
