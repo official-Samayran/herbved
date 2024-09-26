872320a20f8fca888ed0cb897319e7f10462a21e
Samayran <officialsamayran@gmail.com>
2024-09-26 21:04:28 +0530

    Update page.tsx

--- app/page.tsx
+++ app/page.tsx
@@ -1,12 +1,5 @@
-<<<<<<< HEAD
-import { LandingPageComponent } from "@/components/landing-page"
-
-export default function Page() {
-  return <LandingPageComponent />
-=======
 import { HomePage } from "@/components/home-page"
 
 export default function Page() {
   return <HomePage />
->>>>>>> a7e3686 (Initial commit for Virtual Garden project)
 }
